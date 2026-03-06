ddd0d969b5220cd2a79353c96e143d3e0c02467d
Shikhar Negi <shikharnegi31@gmail.com>
2026-03-06 10:52:44 +0530

    initial commit

---
+++ backend/src/dto/signup.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class SignUpDto {
+
+    @IsNotEmpty({ message: 'Username is required' })
+    @IsString({ message: 'Username must be a string' })
+    username: string;
+
+    @IsEmail({}, { message: 'Invalid email format' })
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    @IsString()
+    @IsNotEmpty({ message: 'Password is required' })
+    @MinLength(6, { message: 'Password must be at least 6 characters' })
+    password: string;
+}
